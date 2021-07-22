837c92a32e77e9e6e97f5b5ddad4e5ff1264cd97
Juan F. Gallego <juan.gallego@coassist.com.co>
2021-07-22 16:23:39 -0500

    initial source code import

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zimbrasignatures
+## version 202107221621
+### Juan F Gallego <juan.gallego@coassist.com.co>
 implement an html signature using the same template for all the organization.
+Personalize the html with the custom tags -KEYWORD- adjust bash script to replace the tag with corresponding value and run it.
---
+++ implementar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path="/opt/zimbra/bin"
+
+#for i in `/opt/zimbra/bin/zmprov -l gaa`
+for i in "catalina.arbelaez" "diana.hincapie" "estefania.mora" "yesica.echavarria"
+do
+	echo "======Empezando $i============"
+	f=$(mktemp /tmp/ldata.XXXXXX)
+	cp -f template.html $f
+	zmprov ga $i | egrep "(^uid)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-UID-/@/g' $f
+	zmprov ga $i | egrep "(^givenName)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-GIVENNAME-/@/g' $f
+	zmprov ga $i | egrep "(^sn)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-SN-/@/g' $f
+	zmprov ga $i | egrep "(^title)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-TITLE-/@/g' $f
+	zmprov ga $i | egrep "(^telephoneNumber)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-TELEPHONENUMBER-/@/g' $f
+	zmprov ga $i | egrep "(^mobile)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-MOBILE-/@/g' $f
+	zmprov ma $i zimbraPrefMailSignatureHTML "`cat $f`"
+	echo "============Se modifico $i=============="
+done
+
+
