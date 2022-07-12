4146f501796daa7b7f6a6dc8cae0b36f61d2a586
Juan F. Gallego <juan.gallego@coassist.com.co>
2022-07-12 10:52:54 -0500

    implementar.sh cambio metodo de asignacion de firma

--- implementar.sh
+++ implementar.sh
@@ -11,9 +11,10 @@ do
 	zmprov ga $i | egrep "(^givenName)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-GIVENNAME-/@/g' $f
 	zmprov ga $i | egrep "(^sn)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-SN-/@/g' $f
 	zmprov ga $i | egrep "(^title)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-TITLE-/@/g' $f
-	zmprov ga $i | egrep "(^telephoneNumber)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-TELEPHONENUMBER-/@/g' $f
+	zmprov ga $i | egrep "(^telephoneNumber)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-TELEPHONENUMBER-/604 @/g' $f
 	zmprov ga $i | egrep "(^mobile)"| cut -d : -f 2 | sed 's/^\ //g'|xargs -I@ sed -i 's/-MOBILE-/@/g' $f
-	zmprov ma $i zimbraPrefMailSignatureHTML "`cat $f`"
+	#zmprov ma $i zimbraPrefMailSignatureHTML "`cat $f`"
+	zmprov csig $i firma-coassist1 zimbraPrefMailSignatureHTML "`cat $f`"
 	echo "============Se modifico $i=============="
 done
 
